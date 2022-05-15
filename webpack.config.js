ab330b0be56e63bd180c9290675fd6ae8234cf9b
ally <3210jr@gmail.com>
2022-05-15 18:13:40 +0300

    update tests

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// const webpack = require("webpack");
+// module.exports = (env) => {
+// 	console.log(env); //{NODE_ENV:"dev"}
+// 	return {
+// 		/*original parameters in starter*/
+// 		plugins: [
+// 			new webpack.DefinePlugin({
+// 				"process.env": JSON.stringify({ NODE_ENV: "production", ...env }),
+// 			}),
+// 		],
+// 		entry: "./src/client/index.imba",
+// 	};
+// };
